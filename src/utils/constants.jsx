84a00a529a877bd4c18772be3a4a4a9ddb7355f4
Vijay <rebalvijay930@gmail.com>
2025-02-05 13:30:32 +0530

    updated base url through origin

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,3 @@
-export const Base_URL = window.location.hostname === 'localhost' 
+export const Base_URL = window.location.origin.includes('localhost') 
 ? 'http://localhost:7001'    // Use localhost URL in development
 : '/api' ;      // Use relative path in production
